1d1e83fa5596e93985da99b1dcdf3d0b66b18628
KenneyRogars <kensymonbagos@gmail.com>
2026-04-01 15:23:58 +0800

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-    "https://media.tenor.com/9qR7Wc8Z7pQAAAAi/snoopy-happy.gif",        // 0 normal
-    "https://media.tenor.com/2roX3uxFazcAAAAi/snoopy-confused.gif",     // 1 confused
-    "https://media.tenor.com/YKZ2H8z6s0sAAAAi/snoopy-begging.gif",      // 2 pleading
-    "https://media.tenor.com/3xQ6vK1zW5kAAAAi/snoopy-sad.gif",          // 3 sad
-    "https://media.tenor.com/vs2gX9FjQ6cAAAAi/snoopy-crying.gif",       // 4 sadder
-    "https://media.tenor.com/N7vZl9xBz9cAAAAi/snoopy-depressed.gif",    // 5 devastated
-    "https://media.tenor.com/Fx7hL9g9l9YAAAAi/snoopy-heartbroken.gif",  // 6 very devastated
-    "https://media.tenor.com/1k7sR9k9k9kAAAAi/snoopy-running.gif"       // 7 runaway
+    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
+    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
+    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
+    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
+    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
+    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
+    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
+    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
@@ -67,6 +67,7 @@ function toggleMusic() {
 
 function handleYesClick() {
     if (!runawayEnabled) {
+        // Tease her to try No first
         const msg = yesTeasePokes[Math.min(yesTeasedCount, yesTeasePokes.length - 1)]
         yesTeasedCount++
         showTeaseMessage(msg)
@@ -86,23 +87,28 @@ function showTeaseMessage(msg) {
 function handleNoClick() {
     noClickCount++
 
+    // Cycle through guilt-trip messages
     const msgIndex = Math.min(noClickCount, noMessages.length - 1)
     noBtn.textContent = noMessages[msgIndex]
 
+    // Grow the Yes button bigger each time
     const currentSize = parseFloat(window.getComputedStyle(yesBtn).fontSize)
     yesBtn.style.fontSize = `${currentSize * 1.35}px`
     const padY = Math.min(18 + noClickCount * 5, 60)
     const padX = Math.min(45 + noClickCount * 10, 120)
     yesBtn.style.padding = `${padY}px ${padX}px`
 
+    // Shrink No button to contrast
     if (noClickCount >= 2) {
         const noSize = parseFloat(window.getComputedStyle(noBtn).fontSize)
         noBtn.style.fontSize = `${Math.max(noSize * 0.85, 10)}px`
     }
 
+    // Swap cat GIF through stages
     const gifIndex = Math.min(noClickCount, gifStages.length - 1)
     swapGif(gifStages[gifIndex])
 
+    // Runaway starts at click 5
     if (noClickCount >= 5 && !runawayEnabled) {
         enableRunaway()
         runawayEnabled = true
@@ -137,3 +143,7 @@ function runAway() {
     noBtn.style.top = `${randomY}px`
     noBtn.style.zIndex = '50'
 }
+
+
+
+do the same thing as last time but change the gifs to snoopie themed but still connected to can you be my suitor and if yes happy
